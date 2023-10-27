76ff76869ee938cf174e102c48dfb37b438c13ea
manuel <manuelrondon25@outlook.com>
2023-10-27 17:25:59 -0500

    cambio nombres

--- Patrones_Front/src/app/service/crearEscuela.service.ts
+++ Patrones_Front/src/app/service/crearEscuela.service.ts
@@ -5,14 +5,14 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BackendService {
-  backendUrl = 'http://localhost:8080/cargar'; // URL del backend
+  backendUrl = 'http://localhost:8080/cargar'; 
 
   constructor(private http: HttpClient) { }
 
   enviarDatos(nombreInstitucion: string, file: File) {
     const formData = new FormData();
-    formData.append('nombreInstitucion', nombreInstitucion);
-    formData.append('file', file, file.name);
+    formData.append('nombreEscuela', nombreInstitucion);
+    formData.append('Archivo', file, file.name);
 
     return this.http.post(this.backendUrl, formData);
   }
