5c8cd83bf8938de3f63faf49c49964ed6e045ebc
manuel <manuelrondon25@outlook.com>
2023-10-27 18:49:23 -0500

    cors

--- Patrones_Front/src/app/crear-escuela/crear-escuela.component.ts
+++ Patrones_Front/src/app/crear-escuela/crear-escuela.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { BackendService } from '../service/crearEscuela.service';
+import { FormsModule } from '@angular/forms';
+import { TokenService } from '../service/token.service';
 
 @Component({
-  selector: 'app-escuela',
+  selector: 'app-crear',
   templateUrl: './crear-escuela.component.html',
   styleUrls: ['./crear-escuela.component.css']
 })
 
 export class CrearEscuelaComponent {
-  nombreInstitucion: string = ''; // Variable para almacenar el nombre
+  nombreInstitucion: string = ''; 
   file: File;
+  isLogged = false;
 
-  constructor(private backendService: BackendService) { }
+  constructor(private backendService: BackendService, private tokenService: TokenService) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
 
   onFileSelected(event: any) {
     this.file = event.target.files[0];
@@ -27,7 +38,7 @@ export class CrearEscuelaComponent {
           console.error('Error al enviar los datos:', error);
         }
       );
-    } else {
+    }else {
       console.error('Complete todos los campos.');
     }
   }
